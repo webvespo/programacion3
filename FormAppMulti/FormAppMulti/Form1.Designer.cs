77199700a6b9734e09c2b9de3a666b884417fed2
webvespo <82005304+webvespo@users.noreply.github.com>
2022-04-18 00:38:15 -0300

    Signed-off-by: webvespo <82005304+webvespo@users.noreply.github.com>

---
+++ FormAppMulti/FormAppMulti/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace FormAppMulti
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelBox = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.nuevaVentanaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cerrarProgramaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelBox
+            // 
+            this.panelBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelBox.Location = new System.Drawing.Point(0, 24);
+            this.panelBox.Name = "panelBox";
+            this.panelBox.Size = new System.Drawing.Size(679, 273);
+            this.panelBox.TabIndex = 1;
+            this.panelBox.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevaVentanaToolStripMenuItem,
+            this.cerrarProgramaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(679, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // nuevaVentanaToolStripMenuItem
+            // 
+            this.nuevaVentanaToolStripMenuItem.Name = "nuevaVentanaToolStripMenuItem";
+            this.nuevaVentanaToolStripMenuItem.Size = new System.Drawing.Size(98, 20);
+            this.nuevaVentanaToolStripMenuItem.Text = "Nueva Ventana";
+            this.nuevaVentanaToolStripMenuItem.Click += new System.EventHandler(this.nuevaVentanaToolStripMenuItem_Click);
+            // 
+            // cerrarProgramaToolStripMenuItem
+            // 
+            this.cerrarProgramaToolStripMenuItem.Name = "cerrarProgramaToolStripMenuItem";
+            this.cerrarProgramaToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.cerrarProgramaToolStripMenuItem.Text = "Cerrar";
+            this.cerrarProgramaToolStripMenuItem.Click += new System.EventHandler(this.cerrarProgramaToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 297);
+            this.Controls.Add(this.panelBox);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Formulario Uno";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox panelBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem nuevaVentanaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cerrarProgramaToolStripMenuItem;
+    }
+}
+
---
+++ FormAppMulti/FormAppMulti/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormAppMulti
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cerrarProgramaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void abrirVentanita(object ventanita)
+        {
+            if (this.panelBox.Controls.Count > 0)
+                this.panelBox.Controls.RemoveAt(0);
+            Form fventanita = ventanita as Form;
+            fventanita.TopLevel = false;
+            fventanita.Dock = DockStyle.Fill;
+            this.panelBox.Controls.Add(fventanita);
+            this.panelBox.Tag = fventanita;
+            fventanita.Show();
+        }
+        private void nuevaVentanaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            abrirVentanita(new Ventanita());
+        }
+    }
+}
