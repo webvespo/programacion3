f86114c58eefeab595ac8e132432a6232c5d714a
webvespo <82005304+webvespo@users.noreply.github.com>
2022-06-30 23:46:18 -0300

    Add files via upload

---
+++ Repaso_clase_6/AplicacionWebCoreAspNetApi/AplicacionWebCoreAspNetApi/Controllers/PersonaController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AplicacionWebCoreAspNetApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        public PersonaController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpPost]
+        [Route("GetNombre")]
+        public string Get()
+        {
+            return "Programación 3";
+        }
+    }
+}
---
+++ Repaso_clase_6/AplicacionWebCoreAspNetApi/AplicacionWebCoreAspNetApi/Controllers/VehiculoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AplicacionWebCoreAspNetApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehiculoController : ControllerBase
+    {
+        public VehiculoController()
+        {
+        }
+        [HttpPost]
+        [Route("GetBrand")]
+        public string GetBrandById(long idBrand)
+        {
+            return idBrand == 1 ? "Chevrolet" : "Otras";
+        }
+
+    }
+}
